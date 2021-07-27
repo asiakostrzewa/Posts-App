e37051786d693858ce389eba506411a9a86d5699
asiakostrzewa <asia.kalmus@gmail.com>
2021-07-27 20:50:08 +0200

    Styles added.

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,20 +1,82 @@
 <template>
-  <div>
-  <h2>{{ post.title }}</h2>
-  <p>{{ post.body }}</p>
-  <h3>{{ post.author }}</h3>
-  </div>
+  <article class="wrapper">
+  <h2 class="title">{{ post.title }}</h2>
+  <div class="description" :class="{ expanded : isExpanded }">{{ post.body }}</div>
+  <button class="read-more-btn" v-on:click="toggleText">
+  {{ isExpanded ? "show less" : "read more" }}</button>
+  <h3 class="author">{{ post.author }}</h3>
+  </article>
 </template>
 
 <script>
+
 export default {
   name: 'Post',
   props: {
     post: { type: Object },
   },
+  data() {
+    return {
+      isExpanded: false,
+    };
+  },
+  methods: {
+    toggleText() {
+      this.isExpanded = !this.isExpanded;
+    },
+  },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
 
+  .wrapper {
+    background-color: white;
+    margin: 0 0 20px 0;
+    padding: 10px;
+    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),0 4px 6px -2px rgba(0, 0, 0, 0.05);
+    text-align: center;
+  }
+
+  div {
+    color: $text-color;
+    text-align: center;
+  }
+
+  .title {
+    font-size: 22px;
+  }
+
+  .description {
+    font-weight: 300;
+    width: 90%;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+
+  .expanded {
+    white-space: initial;
+  }
+
+  .read-more-btn {
+    outline: none;
+    border: none;
+    background-color: transparent;
+    margin: 10px 0;
+    color: $main-color;
+  }
+
+  .title::first-letter,
+  .description::first-letter {
+    text-transform: uppercase;
+  }
+
+  .author {
+    font-size: 14px;
+    font-weight: 300;
+    font-style: italic;
+    text-align: right;
+  }
 </style>
--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,5 +1,6 @@
 <template>
-  <ul>
+<h1>Posts App : Home</h1>
+  <ul class="list">
     <li v-for="post in posts" :key="post.id">
 <Post :post="post" />
 </li>
@@ -33,3 +34,30 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
+
+h1 {
+  color: $main-color;
+  text-transform: capitalize;
+  font-weight: 700;
+  font-size: 2.25rem;
+  line-height: 2.5rem;
+  text-align: center;
+  margin-bottom: 2rem;
+}
+
+.list {
+  list-style: none;
+  padding: 0;
+  max-width: 100%;
+  display: grid;
+}
+
+@media (min-width: $tablet) {
+  .list {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+</style>
